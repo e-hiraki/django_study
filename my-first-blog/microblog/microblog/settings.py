03cc92f6065f68183b7761fa0feeae6f9f8fec6b
e-hiraki <ba0760hk@gmail.com>
2022-12-19 20:28:27 +0900

    Update settings.py

--- my-first-blog/microblog/microblog/settings.py
+++ my-first-blog/microblog/microblog/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'microblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sample_app',
+        'USER': 'root',  
+        'PASSWORD': 'pass',
     }
 }
 
